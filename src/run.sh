8778905f1c5896b5abfd4a6b412b93ac989c4ea1
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-03-31 19:48:52 +0900

    ahfbjshdbvkhf

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-git clone https://github.com/AzmtMrzbkv/Software-Engineering---CSE36401
+git clone https://AzmtMrzbkv:fh4k63nr123@github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
 cd Software-Engineering---CSE36401
 mvn install
-# we have to add here java thing, dont know what it is currently 
+# we have to add here java thing, dont know what it is currently
+echo 'run.sh running successfully'
