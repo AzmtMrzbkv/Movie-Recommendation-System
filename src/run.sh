2e1bbda0f79576a3f9448a9bbd7b78a4be63a7e9
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-03-31 20:51:04 +0900

    sffsd

--- src/run.sh
+++ src/run.sh
@@ -4,4 +4,4 @@ git clone https://AzmtMrzbkv:fh4k63nr123@github.com/AzmtMrzbkv/Software-Engineer
 cd Software-Engineering---CSE36401
 mvn install
 # we have to add here java thing, dont know what it is currently
-echo 'run.sh running successfully'
+echo 'run.sh is running successfully'
