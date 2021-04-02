8ccb096e436c433f4c621116495b98e05eac2b64
nodirkh <nodirkhujaev@gmail.com>
2021-04-02 21:21:11 +0900

    pom.xml finished

--- src/run.sh
+++ src/run.sh
@@ -3,4 +3,4 @@
 git clone https://AzmtMrzbkv:fh4k63nr123@github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
 cd Software-Engineering---CSE36401
 mvn install
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar Hello
+java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar RateMeter Adventure educator 
