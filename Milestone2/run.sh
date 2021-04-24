091129a0be6b41f0f56491975e8ca81ba66c514d
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-04-25 00:23:37 +0900

    starting the second project

---
+++ Milestone2/run.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+git clone https://AzmtMrzbkv:fh4k63nr123@github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
+cd Software-Engineering---CSE36401
+mvn install
+java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar RateMeter Adventure educator 
