ababc8dae600be6bbd3a97e89814b38a44785159
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-04-25 16:27:45 +0900

    preparation

--- Milestone2/cse364project/run.sh
+++ Milestone2/cse364project/run.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
 
 git clone https://github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
+
 cd Software-Engineering---CSE36401/Milestone2/
+
 mvn install
+
 java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar com.recommend.YourMainClass.xyx.Tops “” “” “”
 java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar com.recommend.YourMainClass.xyx.Tops “” “” “” “Adventure” 
