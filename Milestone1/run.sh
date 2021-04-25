efdbcdd7759d48faafd24a68c3e3f4706c208ae8
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-04-25 16:25:42 +0900

    preparation

--- Milestone1/run.sh
+++ Milestone1/run.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-git clone https://github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
-cd Software-Engineering---CSE36401/Milestone2/
+git clone https://AzmtMrzbkv:fh4k63nr123@github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
+cd Software-Engineering---CSE36401
 mvn install
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar com.recommend.YourMainClass.xyx.Tops “” “” “”
-java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar com.recommend.YourMainClass.xyx.Tops “” “” “” “Adventure” 
+java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar RateMeter Adventure educator 
