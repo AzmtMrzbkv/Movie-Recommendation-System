90453b7a7f4defac8213fa700ac772534e70f780
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-05-02 20:39:35 +0900

    run.sh fix for Milestone1

--- Milestone1/run.sh
+++ Milestone1/run.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 git clone https://AzmtMrzbkv:fh4k63nr123@github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
-cd Software-Engineering---CSE36401
+cd Software-Engineering---CSE36401/Milestone1
 mvn install
 java -cp target/cse364-project-1.0-SNAPSHOT.jar RateMeter Adventure educator 
