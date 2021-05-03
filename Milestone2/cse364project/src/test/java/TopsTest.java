e6f6d33f159f30711a4eb49b4f39cc537c2f438e
nodirkh <nodirkhujaev@gmail.com>
2021-05-03 18:37:18 +0900

    TestMethods

--- Milestone2/cse364project/src/test/java/TopsTest.java
+++ Milestone2/cse364project/src/test/java/TopsTest.java
@@ -1,5 +1,29 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
+import recommend.Tops;
 
 public class TopsTest {
+    @Test
+    public void testIsValidInput() {
+        Tops tester = new Tops();
+    }
 
+    @Test
+    public void testIsGender() {
+        Tops tester = new Tops();
+        assertEquals("result", true, tester.isGender("M"));
+        assertEquals("result", false, tester.isGender("f"));
+        assertEquals("result", true, tester.isGender("F"));
+        assertEquals("result", false, tester.isGender(""));
+    }
+
+    @Test
+    public void testIsValidAge() {
+        Tops tester = new Tops();
+        assertEquals("result", true, tester.isValidAge("5"));
+        assertEquals("result", true, tester.isValidAge("0"));
+        assertEquals("result", true, tester.isValidAge("57"));
+        assertEquals("result", false, tester.isValidAge("Ffsfs"));
+        assertEquals("result", false, tester.isValidAge("-231"));
+    }
 }
