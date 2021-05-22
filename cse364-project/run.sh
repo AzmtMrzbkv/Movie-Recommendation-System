9a594acda2e5a408abbe5035f13fe2a0d129feb0
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-05-22 14:25:30 +0900

    starting Milestone 3

---
+++ cse364-project/run.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+git clone https://github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
+
+cd ./Software-Engineering---CSE36401/cse364-project
+
+mvn package
+
+java -jar target/RecS-1.0-SNAPSHOT.jar recommend.Tops "" "" ""
