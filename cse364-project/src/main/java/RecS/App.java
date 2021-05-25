7b02fc2b9a195bb4ed1800f22b1ae38db29f1778
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-05-25 20:21:41 +0900

    genre & genres description fault found

--- cse364-project/src/main/java/RecS/App.java
+++ cse364-project/src/main/java/RecS/App.java
@@ -13,19 +13,17 @@ public class App {
     //here will be functions mapped to GET
 
     @GetMapping("/users/recommendations")
-    public Users/*List<Movies>*/ recommendForUser(@RequestBody Users newUser){
+    public List<Movies> recommendForUser(@RequestBody Users newUser){
         // this part to be implemented
         String age = newUser.getAge();
         String genre = newUser.getGenres();
         String gender = newUser.getGender();
         String occupation = newUser.getOccupation();
 
-        return new Users(gender, age, occupation, genre);
-
 //        // check the input validity
-//        if(!Recommender.isValidInput(gender, age, occupation, genre)) { return null;}
-//
-//        return Recommender.limitedTop(Recommender.gradeMovies(new String[]{gender, age, occupation, genre}));
+        if(!Recommender.isValidInput(gender, age, occupation, genre)) { return null;}
+
+        return Recommender.limitedTop(Recommender.gradeMovies(new String[]{gender, age, occupation, genre}));
     }
 
     @GetMapping("/movies/recommendations")
