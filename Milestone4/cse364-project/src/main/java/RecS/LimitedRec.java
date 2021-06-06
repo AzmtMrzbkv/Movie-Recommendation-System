7727004f881773903e696e7ec3601fe1b71fa515
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-06-06 19:43:54 +0900

    added java classes from previous milestone & data dir

---
+++ Milestone4/cse364-project/src/main/java/RecS/LimitedRec.java
@@ -0,0 +1,19 @@
+package RecS;
+
+public class LimitedRec {
+    private final String title;
+    private final String limit;
+
+    public LimitedRec(String title, String limit) {
+        this.title = title;
+        this.limit = limit;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLimit() {
+        return limit;
+    }
+}
