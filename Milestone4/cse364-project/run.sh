655b0e64660e5a312d3cdab73b40867c663fcca7
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-06-19 08:03:37 +0900

    problem mb is caused by my PC

--- Milestone4/cse364-project/run.sh
+++ Milestone4/cse364-project/run.sh
@@ -2,7 +2,7 @@
 
 git clone -b aza-branch https://github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git Milestone4
 
-cd ./Software-Engineering---CSE36401/Milestone4/cse364-project
+cd ./Milestone4/cse364-project
 
 mvn package
 
