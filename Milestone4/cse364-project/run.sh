0bd441bac351da83181c7eb60c596c5c0134dc5e
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-06-19 09:07:06 +0900

    checking using docker

--- Milestone4/cse364-project/run.sh
+++ Milestone4/cse364-project/run.sh
@@ -4,6 +4,8 @@ git clone -b aza-branch https://github.com/AzmtMrzbkv/Software-Engineering---CSE
 
 cd ./Software-Engineering---CSE36401/Milestone4/cse364-project
 
+
 mvn package
+mongod --fork --syslog --dbpath src/main/resources
 
 java -jar target/RecS-1.0-SNAPSHOT.jar
