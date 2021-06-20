827da355a9a77ae7a0f3a915222fa3e46366df41
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-06-20 19:19:01 +0900

    run.sh modified

--- Milestone4/cse364-project/run.sh
+++ Milestone4/cse364-project/run.sh
@@ -4,5 +4,5 @@ git clone https://github.com/AzmtMrzbkv/Software-Engineering---CSE36401.git
 
 cd ./Software-Engineering---CSE36401/Milestone4/cse364-project || exit
 
-#mongod --fork --syslog --dbpath src/main/resources
+mongod --fork --syslog --dbpath src/main/resources
 mvn clean spring-boot:run
