2e9faabee3dcb71cce6b2621bf915e9a6dbcd331
AzmtMrzbkv <myrzabekoffazamat@gmail.com>
2021-06-21 22:39:16 +0900

    added cross orgins

--- Milestone4/cse364-project/src/main/resources/public/users/GetUserRec.js
+++ Milestone4/cse364-project/src/main/resources/public/users/GetUserRec.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/users/recommendation" + $_POST['']
-    }).then(function(data) {
-        $('.gender').append(data.gender);
-        $('.age').append(data.age);
-        $('.occupation').append(data.occupation);
-        $('.genre').append(data.genre);
+    $("#search-form").submit(function (event) {
+        event.preventDefault();
+        let url = $(this).attr("action"); //get form action url
+        let method = $(this).attr("method"); //get form GET/POST method
+        let data = new FormData(this); //Encode form elements for submission
+
+        var str = "";
+        data.forEach(function(value, key){
+            if(str != "")str += "&";
+            str += key + "=" + value;
+        });
+
+        url += '?' + str;
+        console.log(str, method, url);
+        $.ajax({
+            url : url,
+            type: method,
+            dataType: "jsonp",
+
+        }).done(function(response){
+            $('#springResponse').html(response);
+            console.log(response,"UNIStvdfsg");
+        }).fail(function(er){
+            console.log(er);
+        });
     });
 });
